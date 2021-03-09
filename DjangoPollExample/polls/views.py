71f676b1d7056355a5fd49bbf9ceb5c0d6f7cbff
NiCrook <nicrook@comcast.net>
2021-03-08 21:57:52 -0600

    added detail, results, and vote, functions to views.py

--- DjangoPollExample/polls/views.py
+++ DjangoPollExample/polls/views.py
@@ -4,3 +4,16 @@ from django.shortcuts import render
 
 def index(request):
     return HttpResponse("Hello! Welcome to the polls!")
+
+
+def detail(request, question_id):
+    return HttpResponse("You are currently viewing question: %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You are currently viewing the results of question: %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You are currently voting on question: %s" % question_id)
